2c4e69464079abdaccaf072ed1f1b43cbb732e01
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-25 14:44:08 +0500

    Initial Commit

---
+++ ScrollToTop/ScrollToTopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollToTopApp.swift
+//  ScrollToTop
+//
+//  Created by Elexoft on 25/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrollToTopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
