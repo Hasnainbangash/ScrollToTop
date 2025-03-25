ef4c80744eb6ad9a5a70d0a502c2fc42c4eeb713
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-25 14:53:30 +0500

    The 10 listitem are added to the list wrapped in a zstack

--- ScrollToTop/ContentView.swift
+++ ScrollToTop/ContentView.swift
@@ -13,16 +13,27 @@ struct ContentView: View {
     // MARK: - FUNCTION
     
     func listItem(_ num: Int) -> some View {
-        Text("This is item\(num)")
-            .padding(10)
+        Text("This is item \(num)")
+            .padding(50)
     }
     
     // MARK: - BODY
     
     var body: some View {
-        List {
-            
-        } //: LIST
+        ZStack {
+            List {
+                listItem(1)
+                listItem(2)
+                listItem(3)
+                listItem(4)
+                listItem(5)
+                listItem(6)
+                listItem(7)
+                listItem(8)
+                listItem(9)
+                listItem(10)
+            } //: LIST
+        } //: ZSTACK
     }
 }
 
